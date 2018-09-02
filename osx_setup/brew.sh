2999cbf28f66eda6c320798098ca5029e98d8d8d
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2018-09-02 07:04:56 +0300

    Delete osx_setup

--- osx_setup/brew.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh gcc macvim python2 python3 pypy pypy3 youtube-dl ffmpeg
-
-xcode-select --install
-
