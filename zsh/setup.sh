1aaf991e425bd95d56d2708601b1736b5bad8220
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2018-11-29 04:15:27 +0300

    Add zsh setup script

---
+++ zsh/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+
+echo -e "Install 'powerlevel9k' theme with it? (y/n): "
+read choice
+if [[ $choice == "y" ]]; then
+    git clone https://github.com/bhilburn/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    rcfile="macos.zshrc"
+else
+    rcfile="debian.zshrc"
+fi
+cd $(dirname $0)
+cat $rcfile > ~/.zshrc
