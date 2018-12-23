218fb4fdbea5c1b83d8b307ace921943d44ca4ff
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2018-12-23 21:48:24 +0300

    Update init.el for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,23 +1,31 @@
 (package-initialize)
 
 (add-hook 'emacs-startup-hook (lambda ()
-				(setq ns-pop-up-frames nil)				
-				(tool-bar-mode -1)
+				(setq ns-pop-up-frames nil
+				      column-number-mode t
+				      indent-tabs-mode nil
+				      tab-width 4
+				      default-directory "~/")
+				(define-key global-map [remap list-buffers] 'buffer-menu-other-window)
+
+			        (tool-bar-mode -1)
 				(scroll-bar-mode -1)
 				(when window-system (set-frame-size (selected-frame) 91 65))
-				(setq column-number-mode t)
-				(setq indent-tabs-mode nil)
-				(setq tab-width 4)
-				(setq default-directory "~/")
-				(load-theme 'adwaita)
-				(define-key global-map [remap list-buffers] 'buffer-menu-other-window)))
+				(load-theme 'adwaita)))
+
 
 
 (setq python-shell-interpreter "python3")
 (add-hook 'python-mode-hook (lambda ()
-			      (setq-default python-indent 4)
-			      (elpy-enable)
-			      (when window-system (set-frame-size (selected-frame) 160 65))
-			      (split-window-right)))
+			      (setq python-indent 4)
+			      (when window-system (set-frame-size (selected-frame) 162 65))
+			      (split-window-right)
+			      (elpy-enable)))
+
+(add-hook 'c-mode-hook (lambda ()
+			 (setq c-default-style 'linux
+			       c-basic-offset 2)))
 
 
+(add-hook 'makefile-mode-hook (lambda()
+				(setq indent-tabs-mode t)))
