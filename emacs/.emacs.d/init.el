d91229d79be449900d7e16e358d3bb46ecafe187
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-01-04 03:41:29 +0300

    Update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,31 +1,40 @@
 (package-initialize)
 
 (add-hook 'emacs-startup-hook (lambda ()
-				(setq ns-pop-up-frames nil
-				      column-number-mode t
-				      indent-tabs-mode nil
-				      tab-width 4
-				      default-directory "~/")
-				(define-key global-map [remap list-buffers] 'buffer-menu-other-window)
+								(setq-default ns-pop-up-frames nil
+											  column-number-mode t
+											  tab-width 4
+											  default-directory "~/"
+											  )
+				
+								(define-key global-map [remap list-buffers] 'buffer-menu-other-window)
+								(tool-bar-mode -1)
+								(scroll-bar-mode -1)
+								(when window-system (set-frame-size (selected-frame) 91 65))
+								(load-theme 'adwaita)
+								))
 
-			        (tool-bar-mode -1)
-				(scroll-bar-mode -1)
-				(when window-system (set-frame-size (selected-frame) 91 65))
-				(load-theme 'adwaita)))
 
-
-
-(setq python-shell-interpreter "python3")
 (add-hook 'python-mode-hook (lambda ()
-			      (setq python-indent 4)
-			      (when window-system (set-frame-size (selected-frame) 162 65))
-			      (split-window-right)
-			      (elpy-enable)))
+							  (setq indent-tabs-mode nil
+									python-indent 4
+									elpy-rpc-python-command "~/.emacs.d/py3venv/bin/python3"
+									)
+			      
+							  (when window-system (set-frame-size (selected-frame) 162 65))
+							  (split-window-right)
+							  (elpy-enable)
+							  ))
 
-(add-hook 'c-mode-hook (lambda ()
-			 (setq c-default-style 'linux
-			       c-basic-offset 2)))
+
+(add-hook 'c-mode-hook (setq c-default-style 'linux
+							 c-basic-offset 2
+							 indent-tabs-mode nil))
 
 
-(add-hook 'makefile-mode-hook (lambda()
-				(setq indent-tabs-mode t)))
+(add-hook 'makefile-mode-hook (setq indent-tabs-mode t))
+
+
+(add-hook 'latex-mode-hook (setq indent-tabs-mode t
+								 LaTeX-indent-level 4
+								 LaTeX-item-indent 2))
