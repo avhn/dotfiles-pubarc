5cfaafb39b03141452e8f1788469872cb7963814
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-01-11 11:11:01 +0300

    Update emacs setup script
    - Change virtualenv specific 3.7 to 3
    - Rearrange aggregation of code chunks

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -6,17 +6,18 @@ cp -rf $(dirname $0)/.emacs.d $HOME
 
 # create virtualenv
 pip3 install virtualenv
-virtualenv --python=python3.7 $py3venv_dir
+virtualenv --python=python3 $py3venv_dir
 
 # install elpy packages
 source $py3venv_dir/bin/activate
 pip install jedi rope autopep8 yapf black flake8
 deactivate
-
 echo "* Don't forget to install elpy"
-exit
 
 # make emacs default text editor
 #if [[ $OSTYPE == "darwin"* ]]; then
 #  defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.emacs.3;}'
 #fi
+
+exit
+
