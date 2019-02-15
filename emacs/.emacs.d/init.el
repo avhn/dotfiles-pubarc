e4aa993ca41b7005fbead1018744155e92e76b39
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-02-15 11:15:55 +0300

    Update emacs config at init.el   - Fix indentation   - Set default font   - Fix typo, misteriso to misterioso   - Handle exec-path-from-shell-initialize

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,47 +1,56 @@
 (package-initialize)
 
 (add-hook 'emacs-startup-hook (lambda ()
-								(setq-default ns-pop-up-frames nil
-											  column-number-mode t
-											  tab-width 4
-											  default-directory "~/"
-											  python-shell-interpreter "python3"
-											  python-shell-interpreter-args "-i"
-											  )
+  (setq-default ns-pop-up-frames nil
+			    column-number-mode t
+				tab-width 4
+				default-directory "~/"
+				python-shell-interpreter "python3"
+				python-shell-interpreter-args "-i")
 
-								;; if macos-gui, initialize exec-path-from-shell
-								(when (memq window-system '(mac ns x))
-											(exec-path-from-shell-initialize))
-				
-								(define-key global-map [remap list-buffers] 'buffer-menu-other-window)
-								(tool-bar-mode -1)
-								(scroll-bar-mode -1)
-								(when window-system (set-frame-size (selected-frame) 91 65))
-								(load-theme 'adwaita)
-								))
+  (set-default-font "Source Code Pro")
+  
+  ;; if macOS-gui, initialize exec-path-from-shell
+  (if (eq system-type 'darwin)
+    (when (memq window-system '(mac ns x))
+  	    (exec-path-from-shell-initialize)))
+
+  (define-key global-map [remap list-buffers] 'buffer-menu-other-window)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (when window-system (set-frame-size (selected-frame) 91 65))
+  (load-theme 'misterioso t)
+  ))
+
 
 (add-hook 'python-mode-hook (lambda ()
-							  (setq indent-tabs-mode nil
-									python-indent 4
-									;; use virtualenv for elpy packages
-									elpy-rpc-python-command "~/.emacs.d/py3venv/bin/python3.7"
-									)
+  (setq indent-tabs-mode nil
+		python-indent 4)
+
+  ;; use virtualenv for elpy packages
+  (setq elpy-rpc-python-command "~/.emacs.d/py3venv/bin/python3.7")
 			      
-							  (when window-system (set-frame-size (selected-frame) 162 65))
-							  (split-window-right)
-							  (elpy-enable)
-							  ))
+  (when window-system (set-frame-size (selected-frame) 162 65))
+  (split-window-right)
+  (elpy-enable)
+  ))
 
 
-(add-hook 'c-mode-hook (setq c-default-style "linux"
-							 c-basic-offset 2
-							 indent-tabs-mode nil))
+(add-hook 'c-mode-hook (lambda()
+  (setq c-default-style "linux"
+		c-basic-offset 2
+		indent-tabs-mode nil)
+  ))
 
 
-(add-hook 'makefile-mode-hook (setq indent-tabs-mode t))
+(add-hook 'latex-mode-hook (lambda()
+  (setq indent-tabs-mode t
+		LaTeX-indent-level 4
+		LaTeX-item-indent 2)
+  ))
 
+(add-hook 'makefile-mode-hook (lambda()
+  (setq indent-tabs-mode t)))
 
-(add-hook 'latex-mode-hook (setq indent-tabs-mode t
-								 LaTeX-indent-level 4
-								 LaTeX-item-indent 2))
-
+(add-hook 'emacs-lisp-mode-hook (lambda()
+  (setq indent-tabs-mode nil)))
