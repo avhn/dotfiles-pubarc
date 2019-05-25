c939d2a171defeb6a55f1a4731e85792dd380bcb
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-05-25 17:06:54 +0300

    Update emacs setup script

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -6,18 +6,14 @@ cp -rf $(dirname $0)/.emacs.d $HOME
 
 # create virtualenv
 pip3 install virtualenv
-virtualenv --python=python3 $py3venv_dir
+python3 -m virtualenv --python=python3 $py3venv_dir
 
-# install elpy packages
+# set up emacs packages
+emacs -nw --load $(dirname $0)/setup_packages.el
+
+# set up elpy's python dependency packages
 source $py3venv_dir/bin/activate
 pip install jedi rope autopep8 yapf black flake8
 deactivate
-echo "* Don't forget to install elpy"
-
-# make emacs default text editor
-#if [[ $OSTYPE == "darwin"* ]]; then
-#  defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.emacs.3;}'
-#fi
 
 exit
-
