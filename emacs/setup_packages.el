3ea8ecf608ae9cdc395ee534107df30ec0caaac5
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-11-30 01:02:59 +0300

    Move emacs config to it's own repository

--- emacs/setup_packages.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(package-refresh-contents)
-
-(package-install 'exec-path-from-shell)
-(package-install 'elpy)
-
-(save-buffers-kill-emacs)
