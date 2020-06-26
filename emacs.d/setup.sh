b1376538acc8401631589677dcc5478f6cb3305c
Anaxilaus <mertdede@protonmail.com>
2020-06-26 18:32:17 +0300

    Update emacs.d/setup.sh

--- emacs.d/setup.sh
+++ emacs.d/setup.sh
@@ -1,25 +1,22 @@
-#!/usr/bin/env bash
-RED="\033[0;31m"
-
+cd $(dirname $0)
 if [[ ! -f $HOME/.emacs.d && ! -d $HOME/.emacs.d ]]; then
-    echo "Copying files"
-    cp -r $(dirname $0) $HOME/.emacs.d
+    echo "Symlinking files"
+    ln -sfn $(pwd) $HOME/.emacs.d
 else
-    echo "Skipping copying (there's already a file or directory exists at $HOME/.emacs.d)"
+    echo "Skipping symlink (there's already a file or directory exists at $HOME/.emacs.d)"
 fi
-
+
 # install golang.org's language server
-if [[ "$(which go)" != "" ]]; then # check path
+if type go > /dev/null; then
    go get -u golang.org/x/tools/gopls
 else
-    echo "${RED}Golang binary not found in path"
+    echo "Golang binary not found in path"
 fi
 
 # use pandoc for markdown conversion
-if [[ ! $(which pandoc) ]]; then
-    if [[ ! $OS_TYPE == "darwin"* ]] || [[ ! $(which brew) ]] \
-           || [[ ! $(brew install pandoc) ]]; then
-        echo "${RED}pandoc not installed, install for markdown support"
+if ! type pandoc > /dev/null; then
+    if [[ ! $OS_TYPE == "darwin"* ]] || [[ ! $(brew install pandoc) ]]; then
+        echo "pandoc not installed, install for markdown support"
     fi
 fi
 
