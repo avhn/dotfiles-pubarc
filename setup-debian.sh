99f16e8382d46e4c2ba1e3cd408d935038715188
Mert Dede <mertdede@protonmail.com>
2020-07-12 02:33:31 +0300

    Add setup-debian.sh

---
+++ setup-debian.sh
@@ -0,0 +1,17 @@
+## user isolated setup which uses $HOME/bin, use chown -R and chmod -R go-rwx
+## on home folder afterwards
+## WARNING: config files are intended for macos and this script is not tested
+## extensively, if you decide to use this be prepared to fix some things
+sudo apt install git
+# zsh
+zsh ./zsh/setup.sh
+source ./zsh/zprofile
+source ./zsh/zshrc
+# zprofile may not be initialized first by the os depending on the distro
+# rearrange zshrc if needed by the following commands
+#echo "source $HOME/.zprofile" > $HOME/.zshrc
+#cat ./zsh/zshrc >> $HOME/.zshrc
+# homebrew, emacs, gpg, go, npm etc.
+mkdir -p $HOME/bin/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/bin/homebrew
+brew install emacs gpg go npm python3.8
+zsh ./emacs.d/setup.sh
