e22e2cfb7119d7bee475156d16732441b00d77e1
Mert Dede <mertdede@protonmail.com>
2022-02-17 02:42:05 +0300

    Update setup-debian.sh and add bashrc

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,13 +1,18 @@
 ## user isolated setup which uses $HOME/bin, use chown -R and chmod -R go-rwx
 ## on home folder afterwards
 cd $(dirname $0)
-## TODO: add contrib and non-free to /etc/apt/sources.list
+## TODO: add contrib and non-free to /etc/apt/sources.list (? universe ? for firacode)
 sudo apt update
 sudo apt upgrade -y
 # install drivers
 sudo apt install linux-headers-amd64 linux-image-amd64 nvidia-driver
 # set up fonts
-sudo apt install fonts-noto
-## TODO: set up linuxbrew/homebrew
+sudo apt install fonts-noto fonts-firacode
+## set up linuxbrew/homebrew
 sudo apt install curl
-mkdir -p $HOME/bin/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/bin/homebrew
+mkdir -p $HOME/brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/brew
+# set up oh-my-bash
+git clone https://github.com/ohmybash/oh-my-bash $HOME/.oh-my-bash
+
+brew install diff-so-fancy
+
